f666bc1c9931e819a9a63e5bc14de817fee5860d
ParkHyeongGwon <gudrnjs0503@naver.com>
2024-02-02 17:20:25 +0900

    FindStation 수정 (useState, useEffect)

--- ProjectFrontEnd/client/src/components/FindStation/App.js
+++ ProjectFrontEnd/client/src/components/FindStation/App.js
@@ -1,38 +1,25 @@
 /*global kakao*/
-import React, { Component } from 'react';
-import jsonData from "./map_info.json";
+import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
 import './App.css';
 import axios from "axios";
-const { kakao1 } = window;
+const { kakao } = window;
 
-class App extends Component {
+const App = () => {
 
-  constructor(props) {
-    super(props);
-    // API 응답 데이터를 저장할 State
-    this.state = {
-      responseFPList: '',
-      append_FPList: '',
-    }
-  }
+  const [responseStationList, setresponseStationList] = useState('');
 
-  // 컴포넌트가 마운트될 때 API 호출
-  componentDidMount() {
-    this.callStationListApi()
-  }
+  useEffect(() => {
+    callStationListApi()
+  }, []);
 
-  // API에서 충전소 데이터를 가져옴
-  callStationListApi = async () => {
+  const callStationListApi = () => {
     axios.get('http://localhost:8000/api/charging-stations/', {
     })
       .then(response => {
         try {
-          // API 응답 데이터를 컴포넌트 State에 설정
-          this.setState({ responseStationList: response });
-
-          // 충전소 목록을 지도에 추가하는 메소드 호출
-          this.setState({ append_StationList: this.StationListAppend() });
+          setresponseStationList(response.data);
+          StationListAppend(response.data);
         } catch (error) {
           alert(error)
         }
@@ -40,8 +27,7 @@ class App extends Component {
       .catch(error => { alert(error); return false; });
   }
 
-  // 지도와 마커 클러스터러 초기화
-  StationListAppend = () => {
+  const StationListAppend = (STList) => {
     var mapContainer = document.getElementById('map'),
       mapOption = {
         center: new kakao.maps.LatLng(37.505496, 127.005116),
@@ -54,9 +40,6 @@ class App extends Component {
       minLevel: 5
     });
 
-    // State에서 충전소 목록 데이터 추출
-    let result = []
-    var STList = this.state.responseStationList.data
 
     var markers = $(STList).map(function (i, position) {
       var imageSrc = require("./img/마킹123.gif")
@@ -69,7 +52,6 @@ class App extends Component {
         image: markerImage
       });
 
-      // 각 충전기에 대한 정보 테이블 생성
       var str = '';
       $.each(position.charger, function (index, value) {
 
@@ -129,7 +111,6 @@ class App extends Component {
             chargeStat = '???';
         }
 
-        // 테이블에 충전기 정보 추가
         str +=
           '<tr>' +
           '<td colspan="2" style="background-color: #a4d1ae">충전기[No.' + value.chgerId + ']</td>' +
@@ -143,7 +124,6 @@ class App extends Component {
           '</tr>'
       })
 
-      // 주차 상태 결정
       var parkingStatus;
 
       if (position.parkingFree === 'Y') {
@@ -202,7 +182,6 @@ class App extends Component {
         });
       };
 
-      // 마커에 클릭 이벤트 리스너 추가
       kakao.maps.event.addListener(marker, 'click', clickHandler1)
       return marker;
     })
@@ -212,10 +191,9 @@ class App extends Component {
   }
 
   // 지도 컨테이너 렌더링
-  render() {
     return (
       <div id="map" style={{ "width": "100%", "height": "700px" }}></div>
     )
   }
-}
-export default App;
+
+  export default App;
