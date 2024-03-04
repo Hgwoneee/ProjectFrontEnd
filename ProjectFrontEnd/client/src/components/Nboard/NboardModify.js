099072454f79d9681dbb86899965dd60c3f841ae
ParkHyeongGwon <gudrnjs0503@naver.com>
2024-03-04 12:26:02 +0900

    NboarModify 수정

--- ProjectFrontEnd/client/src/components/Nboard/NboardModify.js
+++ ProjectFrontEnd/client/src/components/Nboard/NboardModify.js
@@ -1,48 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import axios from "axios";
 import $ from 'jquery';
 import Swal from 'sweetalert2'
 
 
-class NboardModify extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bno: props.match.params.bno,
-            selectedFile: null,
-            imageDTOList: [],
-            imageList: [],
-            title: '',
-            content: '',
-            writer: '',
-        }
-    }
+const NboardModify = (props) => {
 
-    componentDidMount() {
-        this.callNboardInfoApi();
+    let history = useHistory();
+
+    const [bno] = useState(props.match.params.bno);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [imageDTOList, setImageDTOList] = useState([]);
+    const [imageList, setImageList] = useState([]);
+    const [title, setTitle] = useState();
+    const [content, setContent] = useState();
+    const [writer, setWriter] = useState();
+
+    useEffect(() => {
+        callNboardInfoApi();
         $('#articleNo').hide();
-    }
-
-    
-    callNboardInfoApi = async () => {
+    }, [])
 
 
+    const callNboardInfoApi = () => {
         axios.post('/api/nBoards/read', {
-            bNo: this.state.bno,
+            bNo: bno
         })
             .then(response => {
                 try {
-                    var data = response.data
-                    this.setState({
-                        title: data.title,
-                        content: data.content,
-                        writer: data.writer,
-                        imageDTOList: data.imageDTOList,
-                        imageList: data.imageDTOList.map(image => ({
-                            thumbnailURL: image.thumbnailURL,
-                        })),
-                    });
+                    setTitle(response.data.title);
+                    setContent(response.data.content);
+                    setWriter(response.data.writer);
+                    setImageDTOList(response.data.imageDTOList);
+                    setImageList(response.data.imageDTOList.map(image => ({
+                        thumbnailURL: image.thumbnailURL
+                    })));
                 }
                 catch (error) {
                     alert('게시글데이터 받기 오류')
@@ -52,9 +45,8 @@ class NboardModify extends Component {
 
     }
 
-    
-    renderImages = () => {
-        const { imageList } = this.state;
+
+    const renderImages = () => {
 
         return imageList.map((image, index) => (
             <li className="hidden_type" key={index}>
@@ -66,24 +58,23 @@ class NboardModify extends Component {
         ));
     };
 
-   
-    submitClick = async (type, e) => {
 
-        
-        this.title_checker = $('#titleVal').val();
-        this.content_checker = $('#contentVal').val();
+    const submitClick = (type, e) => {
 
-        this.fnValidate = (e) => {
-            if (this.title_checker === '') {
+        const title_checker = $('#titleVal').val();
+        const content_checker = $('#contentVal').val();
+
+        const fnValidate = (e) => {
+            if (title_checker === '') {
                 $('#titleVal').addClass('border_validate_err');
-                this.sweetalert('제목을 입력해주세요.', '', 'error', '닫기')
+                sweetalert('제목을 입력해주세요.', '', 'error', '닫기')
                 return false;
             }
             $('#titleVal').removeClass('border_validate_err');
 
-            if (this.content_checker === '') {
+            if (content_checker === '') {
                 $('#contentVal').addClass('border_validate_err');
-                this.sweetalert('내용을 입력해주세요.', '', 'error', '닫기')
+                sweetalert('내용을 입력해주세요.', '', 'error', '닫기')
                 return false;
             }
             $('#contentVal').removeClass('border_validate_err');
@@ -91,27 +82,25 @@ class NboardModify extends Component {
             return true;
         }
 
-        
-        if (this.fnValidate()) {
-            var jsonstr = $("form[name='frm']").serialize();
+
+        if (fnValidate()) {
+            let jsonstr = $("form[name='frm']").serialize();
             jsonstr = decodeURIComponent(jsonstr);
-            var Json_form = JSON.stringify(jsonstr).replace(/\"/gi, '')
+            let Json_form = JSON.stringify(jsonstr).replace(/\"/gi, '')
             Json_form = "{\"" + Json_form.replace(/\&/g, '\",\"').replace(/=/gi, '\":"') + "\"}";
-            var Json_data = {
+            let Json_data = {
                 ...JSON.parse(Json_form),
-                imageDTOList: this.state.imageDTOList,
+                imageDTOList: imageDTOList,
             };
-
-
 
             axios.post('/api/nBoards/modify', Json_data)
                 .then(response => {
                     try {
                         if (response.data == "succ") {
-                            this.sweetalert('수정되었습니다.', '', 'success', '확인')
-                            setTimeout(function () {
-                                this.props.history.push(`/NboardRead/${this.state.bno}`);
-                            }.bind(this), 1500
+                            sweetalert('수정되었습니다.', '', 'success', '확인')
+                            setTimeout(() => {
+                                history.push(`/NboardRead/${bno}`);
+                            }, 1500
                             );
                         }
                     }
@@ -123,7 +112,7 @@ class NboardModify extends Component {
         }
     };
 
-    sweetalert = (title, contents, icon, confirmButtonText) => {
+    const sweetalert = (title, contents, icon, confirmButtonText) => {
         Swal.fire({
             title: title,
             text: contents,
@@ -132,152 +121,113 @@ class NboardModify extends Component {
         })
     }
 
-    sweetalertSucc = (title, showConfirmButton) => {
-        Swal.fire({
-            icon: 'success',
-            title: title,
-            showConfirmButton: showConfirmButton,
-            timer: 1000
-        })
+    const handleFileInput = (type, e) => {
+        const selected = e.target.files[0];
+        $('#imagefile').val(selected ? selected.name : '');
+        setSelectedFile(selected);
     }
 
-    
-    handleFileInput(type, e) {
-        if (type == 'file') {
-            $('#imagefile').val(e.target.files[0].name)
-        } else if (type == 'manual') {
-            $('#manualfile').val(e.target.files[0].name)
+    useEffect(() => {
+        if (selectedFile) {
+            handlePostImage();
         }
-        this.setState({
-            selectedFile: e.target.files[0],
-        })
-        setTimeout(function () {
-            if (type == 'manual') {
-                this.handlePostMenual()
-            } else {
-                this.handlePostImage(type)
-            }
-        }.bind(this), 1
-        );
-    }
+    }, [selectedFile]);
 
-    
-    handlePostImage(type) {
+
+    const handlePostImage = async () => {
         const formData = new FormData();
-        formData.append('uploadFiles', this.state.selectedFile);
-        return axios.post("/uploadAjax", formData).then(res => {
-            if (type == 'file') {
-                // 이미지 정보를 상태에 업데이트
-                this.setState({ fileName: res.data[0].fileName })
-                this.setState({ uuid: res.data[0].uuid })
-                this.setState({ path: res.data[0].folderPath })
-                this.setState({ thumbnailURL: res.data[0].thumbnailURL })
-                this.setState({ imageURL: res.data[0].imageURL })
+        formData.append('uploadFiles', selectedFile);
 
-                var str = "";
+        try {
+            const res = await axios.post("/uploadAjax", formData);
+            const { fileName, uuid, folderPath, thumbnailURL } = res.data[0];
 
-                str += "<li data-name='" + this.state.fileName + "' data-path='" + this.state.folderPath + "' data-uuid='" + this.state.uuid + "'>";
-                str += "<img src='/display?fileName=" + this.state.thumbnailURL + "'>";
-                str += "</li>";
+            setImageDTOList((prevImageDTOList) => [
+                ...prevImageDTOList,
+                { imgName: fileName, path: folderPath, uuid: uuid },
+            ]);
 
-                $('#upload_img').append(str)
-
-                
-                const imageInfo = {
-                    imgName: this.state.fileName,
-                    path: this.state.path,
-                    uuid: this.state.uuid,
-                };
-                this.setState(prevState => ({
-                    imageDTOList: [...prevState.imageDTOList, imageInfo], 
-                }));
-
-            }
-        }).catch(error => {
-            alert('작업중 오류가 발생하였습니다.')
-        })
+            const str = `<li data-name='${fileName}' data-path='${folderPath}' data-uuid='${uuid}'>
+                            <img src='/display?fileName=${thumbnailURL}'>
+                          </li>`;
+            $('#upload_img').append(str);
+        } catch (error) {
+            alert('작업 중 오류가 발생하였습니다.');
+        }
     }
 
-    
-    handleRemoveAllThumbnails = () => {
+    const handleRemoveAllThumbnails = () => {
         $('.fileBox1 ul').empty();
         $('#imagefile').val('');
-        this.setState({ imageDTOList: [] });
+        setImageDTOList([]);
     };
 
-    render() {
-        return (
-            <section class="sub_wrap">
-                <article class="s_cnt mp_pro_li ct1">
-                    <div class="li_top">
-                        <h2 class="s_tit1">게 시 글 수 정</h2>
-                    </div>
-                    <div class="bo_w re1_wrap re1_wrap_writer">
-                        <form name="frm" id="frm" action="" onsubmit="" method="post" >
-                            <article class="res_w">
-                                <div class="tb_outline">
-                                    <table class="table_ty1">
-                                        <tr id="articleNo">
-                                            <th>
-                                                <label for="bNo">글번호</label>
-                                            </th>
-                                            <td>
-                                                <input type="text" name="bNo" id="bNoVal" value={this.state.bno} />
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th>
-                                                <label for="title">제목</label>
-                                            </th>
-                                            <td>
-                                                <input type="text" name="title" id="titleVal" defaultValue={this.state.title} />
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th>
-                                                <label for="Content">내용</label>
-                                            </th>
-                                            <td>
-                                                <textarea style={{ padding: '15px' }} name="content" id="contentVal" rows="" cols="" defaultValue={this.state.content} ></textarea>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th>
-                                                파일첨부
-                                            </th>
-                                            <td className="fileBox fileBox1">
-                                                <label htmlFor='imageSelect' className="btn_file">파일선택</label>
-                                                <input type="text" id="imagefile" className="fileName fileName1"
-                                                    readOnly="readonly" placeholder="선택된 파일 없음" />
-                                                <input type="file" id="imageSelect" className="uploadBtn uploadBtn1"
-                                                    onChange={e => this.handleFileInput('file', e)} multiple />
-                                                <button type="button" className='bt_ty2' style={{ paddingTop: 5, paddingLeft: 10, paddingRight: 10 }}
-                                                    onClick={this.handleRemoveAllThumbnails}>X</button>
-                                                <ul id="upload_img">
-                                                    {this.renderImages()}
-                                                </ul>
-                                            </td>
-                                        </tr>
+    return (
+        <section class="sub_wrap">
+            <article class="s_cnt mp_pro_li ct1">
+                <div class="li_top">
+                    <h2 class="s_tit1">게 시 글 수 정</h2>
+                </div>
+                <div class="bo_w re1_wrap re1_wrap_writer">
+                    <form name="frm" id="frm" action="" onsubmit="" method="post" >
+                        <article class="res_w">
+                            <div class="tb_outline">
+                                <table class="table_ty1">
+                                    <tr id="articleNo">
+                                        <th>
+                                            <label for="bNo">글번호</label>
+                                        </th>
+                                        <td>
+                                            <input type="text" name="bNo" id="bNoVal" value={bno} />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>
+                                            <label for="title">제목</label>
+                                        </th>
+                                        <td>
+                                            <input type="text" name="title" id="titleVal" defaultValue={title} />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>
+                                            <label for="Content">내용</label>
+                                        </th>
+                                        <td>
+                                            <textarea style={{ padding: '15px' }} name="content" id="contentVal" rows="" cols="" defaultValue={content} ></textarea>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>
+                                            파일첨부
+                                        </th>
+                                        <td className="fileBox fileBox1">
+                                            <label htmlFor='imageSelect' className="btn_file">파일선택</label>
+                                            <input type="text" id="imagefile" className="fileName fileName1"
+                                                readOnly="readonly" placeholder="선택된 파일 없음" />
+                                            <input type="file" id="imageSelect" className="uploadBtn uploadBtn1"
+                                                onChange={e => handleFileInput('file', e)} multiple />
+                                            <button type="button" className='bt_ty2' style={{ paddingTop: 5, paddingLeft: 10, paddingRight: 10 }}
+                                                onClick={handleRemoveAllThumbnails}>X</button>
+                                            <ul id="upload_img">
+                                                {renderImages()}
+                                            </ul>
+                                        </td>
+                                    </tr>
 
-                                    </table>
-                                    <div class="btn_confirm mt20" style={{ "margin-bottom": "44px", textAlign: "center" }}>
-                                        <a href="javascript:" className="bt_ty bt_ty2 submit_ty1 saveclass"
-                                            onClick={(e) => this.submitClick('file',
-                                                {
-                                                    fileName: this.state.fileName,
-                                                    folderPath: this.state.folderPath,
-                                                    uuid: this.state.uuid
-                                                }, e)}>저장</a>
-                                        <Link to={`/NboardRead/${this.state.bno}`} className="bt_ty bt_ty2 submit_ty1 saveclass">취소</Link>
-                                    </div>
+                                </table>
+                                <div class="btn_confirm mt20" style={{ "margin-bottom": "44px", textAlign: "center" }}>
+                                    <a href="javascript:" className="bt_ty bt_ty2 submit_ty1 saveclass"
+                                        onClick={(e) => submitClick('file', e)}>저장</a>
+                                    <Link to={`/NboardRead/${bno}`} className="bt_ty bt_ty2 submit_ty1 saveclass">취소</Link>
                                 </div>
-                            </article>
-                        </form>
-                    </div>
-                </article>
-            </section>
-        );
-    }
+                            </div>
+                        </article>
+                    </form>
+                </div>
+            </article>
+        </section>
+    );
 }
 
 export default NboardModify;
